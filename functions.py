e93c3d2c3efa7872d421f2176bcd114556058740
Abhishek <gupta.abhishek11956@gmail.com>
2025-06-22 14:35:32 +0530

    Initial commit

---
+++ functions.py
@@ -0,0 +1,20 @@
+FILEPATH = filepath = "main.txt"
+
+
+
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+
+if __name__ == "__main__":
+     print("hay it a good day!")
+     print(get_todos())
